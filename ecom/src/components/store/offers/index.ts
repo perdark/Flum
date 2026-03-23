5e074e7b0ec8dbd7cc9e6256b74dab313497f0de
Dark <108168861+perdark@users.noreply.github.com>
2026-03-23 05:37:28 +0300

    Delete ecom directory

--- ecom/src/components/store/offers/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Offers Components Export
- */
-
-export { CountdownTimer } from './countdown-timer';
-export { HeroOffers } from './hero-offers';
-export { OfferCards } from './offer-cards';
-export { OfferBanner, MultipleBanners } from './offer-banner';
